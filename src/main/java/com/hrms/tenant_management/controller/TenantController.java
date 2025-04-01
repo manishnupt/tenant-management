43897f4916405df518b850a4deafecadd9510ff2
Manish.mehts <manish.mehts@mynxsoftwares.com>
2025-04-01 23:36:23 +0530

    new controller fix

--- src/main/java/com/hrms/tenant_management/controller/TenantController.java
+++ src/main/java/com/hrms/tenant_management/controller/TenantController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/tenants")
+@CrossOrigin(origins="*")
 public class TenantController {
     @Autowired
     TenantService tenantService;
